c1e812fb38852fc2547110039e5813e109e03f65
Fabio <fabiofarias.de.almeida@gmail.com>
2025-03-15 21:26:06 -0300

    Update 2-streamlit-dashboard-fixed.py

--- 2-streamlit-dashboard-fixed.py
+++ 2-streamlit-dashboard-fixed.py
@@ -372,13 +372,13 @@ st.markdown("""
     /* Estilo do "track" (trilho) da barra de rolagem */
     ::-webkit-scrollbar-track {
         background: #f1f1f1;
-        border-radius: 10px;
+        border-radius: 1px;
     }
     
     /* Estilo do "thumb" (parte móvel) da barra de rolagem */
     ::-webkit-scrollbar-thumb {
         background: #888;
-        border-radius: 10px;
+        border-radius: 20px;
     }
     
     /* Ao passar o mouse */
