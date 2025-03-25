7f0a69d692486379d6d29ac0cb3562bed633549c
Fabio <fabiofarias.de.almeida@gmail.com>
2025-03-25 14:25:21 -0300

    1. Separar o Mapeamento de Colunas em Arquivo JSON Externo e 4. Centralizar Constantes de Texto

--- 2-streamlit-dashboard-fixed.py
+++ 2-streamlit-dashboard-fixed.py
@@ -49,12 +49,6 @@ css_sidebar = """
 
 st.markdown(css_sidebar, unsafe_allow_html=True)
 
-def iniciar_secao_filtros():
-    return st.sidebar.markdown('<div class="filtro-azul">', unsafe_allow_html=True)
-
-def finalizar_secao_filtros():
-    return st.sidebar.markdown('</div>', unsafe_allow_html=True)
-
 # -------------------------------
 # Funções Auxiliares
 # -------------------------------
