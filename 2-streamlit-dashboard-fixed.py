c96f1f717bf1a84797dd423811ce566d99b1aece
Fabio <fabiofarias.de.almeida@gmail.com>
2025-03-29 15:42:34 -0300

    Refatoracao

--- 2-streamlit-dashboard-fixed.py
+++ 2-streamlit-dashboard-fixed.py
@@ -512,7 +512,7 @@ def exibir_tabela_com_aggrid(df_para_exibir, altura=600, coluna_dados=None, posi
                             return {
                                 ...baseStyle,
                                 'font-weight': 'bold',
-                                'background-color': '#e37777'
+                                'background-color': '#F3C5C5'
                             };
                         }
 
@@ -534,7 +534,7 @@ def exibir_tabela_com_aggrid(df_para_exibir, altura=600, coluna_dados=None, posi
                 cellStyle=JsCode("""
                     function(params) {
                         if (params.node.rowPinned) {
-                            return { 'font-weight': 'bold', 'background-color': '#e37777' };
+                            return { 'font-weight': 'bold', 'background-color': '#F3C5C5' };
                         }
                     }
                 """),
