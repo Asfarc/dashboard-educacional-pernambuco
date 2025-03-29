61975c84a71cc38bd8da22e93afdb2f926dae86f
Fabio <fabiofarias.de.almeida@gmail.com>
2025-03-29 15:53:01 -0300

    Refatoracao

--- 2-streamlit-dashboard-fixed.py
+++ 2-streamlit-dashboard-fixed.py
@@ -564,6 +564,7 @@ def exibir_tabela_com_aggrid(df_para_exibir, altura=600, coluna_dados=None, posi
             }
         },
         rowStyle={"textAlign": "center"},
+        cellSelection=True,
         rowSelection='none',
         suppressRowDeselection=True,
         suppressRowClickSelection=True,
