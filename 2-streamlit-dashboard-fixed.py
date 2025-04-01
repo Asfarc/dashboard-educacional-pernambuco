77ee4ade08de81bbc1b45fe73c6a29b1ff8a5eb9
Fabio <fabiofarias.de.almeida@gmail.com>
2025-04-01 19:59:23 -0300

    atualizacao

--- 2-streamlit-dashboard-fixed.py
+++ 2-streamlit-dashboard-fixed.py
@@ -338,7 +338,7 @@ h2 {
     [data-testid="stSidebar"] {
         width: 100% !important;
         margin: 0 !important;
-        padding: 8rem !important;
+        padding: 1rem !important;
     }
 }
 """
