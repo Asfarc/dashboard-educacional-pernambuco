2c39ef5515a291fd5da135accec3e68ef73bd0ac
Fabio <fabiofarias.de.almeida@gmail.com>
2025-04-01 17:35:45 -0300

    atualizacao

--- layout_primeiros_indicadores.py
+++ layout_primeiros_indicadores.py
@@ -11,10 +11,10 @@ alt.renderers.set_embed_options({
     'timeFormatLocale': 'pt_BR'  # Opcional para datas em português
 })
 
-# Dicionário que centraliza parâmetros estilísticos e facilita ajustes
+# Dicionário que centraliza parâmetros estilísticos e facilita ajustes # dee2e6
 PARAMETROS_ESTILO_CONTAINER = {
     "raio_borda": 8,
-    "cor_borda": "#dee2e6",
+    "cor_borda": "#ff0000",
     "cor_titulo": "#364b60",
     "tamanho_fonte_titulo": "1.1rem",
     "cor_fonte_conteudo": "#364b60",
