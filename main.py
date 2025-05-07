647e0a8cb5b6d00bb08cfd3921f50459cfa0af87
Fabio <fabiofarias.de.almeida@gmail.com>
2025-05-07 10:43:45 -0300

    atualizacao

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ except Exception as e:
 
 
 # ─── 6. SIDEBAR – nível de agregação ────────────────────────────────
-st.sidebar.title("Filtross")
+st.sidebar.title(z)
 
 # ▶️ Medidor de memória RAM
 ram_mb = psutil.Process(os.getpid()).memory_info().rss / 1024**2
